f418795540816b47847ed1f418636be87a85b2fb
rabobahago <rabobahago@gmail.com>
2020-07-11 20:38:32 +0100

    more JavaScript codes added

--- Project files/app.js
+++ Project files/app.js
@@ -1,9 +1,17 @@
 //using selectors inside the element
 // traversing the dom
-const btns = document.querySelectorAll('.question-btn');
-btns.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{
-        const question = e.currentTarget.parentElement.parentElement;
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click',(e)=>{
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+const questions = document.querySelectorAll('.question');
+questions.forEach((question)=>{
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', ()=>{
         question.classList.toggle('show-text');
     })
 })
